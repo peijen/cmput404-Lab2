8eb17c4fd030dfa24c1b4dd20f74dd929c75466a
peijen <peijen@ualberta.ca>
2016-09-14 15:52:24 -0600

    done

---
+++ lab2.py
@@ -0,0 +1,83 @@
+import socket
+import os
+# allocate a new socket
+#client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+#Connect to google
+#client.connect(('www.google.ca',80))
+
+
+
+server.bind(('0.0.0.0',8000))   #bind to ur own 
+server.listen(1)
+
+while True:
+	print "waiting for connection"
+	client, address = server.accept()
+	print("connected!")
+	print address
+
+	pid = os.fork()
+	if pid==0: # we are in child process
+            #client is going to be curl, web browser or something similar
+	    outgoing = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	    outgoing.connect(('www.google.ca',80))
+
+	#if no data is coming make it fail
+	    outgoing.setblocking(0)
+	    client.setblocking(0)
+	    while True:
+	   
+	#curl and client are both waiting for response, deadlock 
+	#get the client and forward it google
+		    try:
+			part = client.recv(1024)
+		    except socket.error, exception:
+			if exception.errno ==11:
+			    part =None
+			else:
+			    raise
+		    if (part is not None) and (len(part) ==0):
+			    exit(0)
+		    if part:
+	
+			print "<"+part
+			outgoing.sendall(part)
+		    try:
+			part = outgoing.recv(1024)
+		    except socket.error, exception:
+			if exception.errno ==11:
+			    part =None
+			else:
+			    raise
+		    if (part is not None) and (len(part) ==0):
+			    exit(0)
+		    if part:
+			 
+			print ">"+ part
+			client.sendall(part)
+
+
+
+
+
+
+'''
+#for client
+http = "GET / HTTP/1.0\r\n\r\n"
+
+
+client.sendall(http)
+
+msg =""
+
+while True:
+    part = client.recv(1024)
+    if part:
+        msg += part
+    else:
+        break
+
+print msg
+'''
